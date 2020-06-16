01cb7cdb68f42c63a6b0074b5cd9641ae7ebdc28
m.darin <m.darin.comco@yandex.ru>
2020-06-16 10:46:46 +0300

    fix ambiguity

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ## stop the server
-docker stop $(docker ps |  awk '/^([abcdefABCDEF0123456789]{12,}).+/ {print $1}')
+docker stop $(docker ps |  awk '/^([abcdefABCDEF0123456789]{12,}).+?(apollo-mock-server).+/ {print $1}') 
